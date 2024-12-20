bc1929f3c8cce267536562cd2323950833b95cf0
DinisSimoes <di.f.simoes@gmail.com>
2024-12-19 23:46:09 -0300

    user handlers

---
+++ MyTaskManager.Application/Handlers/Auth/LoginUserCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using MyTaskManager.Application.Commands;
+using MyTaskManager.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTaskManager.Application.Handlers.Auth
+{
+    public class LoginUserCommandHandler : IRequestHandler<LoginUserCommand, string>
+    {
+        private readonly IUserService _userService;
+        private readonly ITokenService _tokenService;
+
+        public LoginUserCommandHandler(IUserService userService, ITokenService tokenService)
+        {
+            _userService = userService;
+            _tokenService = tokenService;
+        }
+
+        public async Task<string> Handle(LoginUserCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userService.GetUserByUsernameAsync(request.Username);
+
+            // Verifica se o usuário existe e valida a senha
+            if (user == null || !BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHash))
+                return null;
+
+            // Gera o token JWT
+            return _tokenService.GenerateToken(user);
+        }
+    }
+}
---
+++ MyTaskManager.Application/Handlers/Auth/RegisterUserCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using MyTaskManager.Application.Commands;
+using MyTaskManager.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTaskManager.Application.Handlers.Auth
+{
+    internal class RegisterUserCommandHandler : IRequestHandler<RegisterUserCommand, Unit>
+    {
+        private readonly IUserService _userService;
+
+        public RegisterUserCommandHandler(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public async Task<Unit> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
+        {
+            if (await _userService.IsUsernameTakenAsync(request.Username))
+                throw new InvalidOperationException("Username is already taken.");
+
+            await _userService.CreateUserAsync(request.Username, request.Password);
+
+            return Unit.Value;
+        }
+    }
+}
