26e1587b8d85fab46b7068e27177449b4ea7ea27
DinisSimoes <di.f.simoes@gmail.com>
2024-12-19 23:46:44 -0300

    demais ficheiros para primeira subida

---
+++ MyTaskManager.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyTaskManager.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTaskManager.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Todo> Todos { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
