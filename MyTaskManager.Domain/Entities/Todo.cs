a4c3f303203a37a8c62b22fd5cce2b131ae25d2b
DinisSimoes <di.f.simoes@gmail.com>
2024-12-19 23:44:26 -0300

    entidades

---
+++ MyTaskManager.Domain/Entities/Todo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTaskManager.Domain.Entities
+{
+    public class Todo
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? CompletedAt { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
