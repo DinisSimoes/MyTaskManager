d303a0e4268bb89d06598176b0e8d6937e034919
DinisSimoes <di.f.simoes@gmail.com>
2024-12-19 23:45:06 -0300

    migrations e infrastruture

---
+++ MyTaskManager.Infrastructure/Migrations/20241219030722_initial.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyTaskManager.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Todos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CompletedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Todos", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Todos_Id_UserId",
+                table: "Todos",
+                columns: new[] { "Id", "UserId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Todos");
+        }
+    }
+}
---
+++ MyTaskManager.Infrastructure/Repositories/TodoRepository.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyTaskManager.Domain.Entities;
+using MyTaskManager.Domain.Interfaces;
+using MyTaskManager.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTaskManager.Infrastructure.Repositories
+{
+    public class TodoRepository : ITodoRepository
+    {
+        private readonly AppDbContext _dbContext;
+
+        public TodoRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Todo?> GetByIdAsync(Guid id)
+        {
+            return await _dbContext.Todos
+                .FirstOrDefaultAsync(t => t.Id == id);
+        }
+
+        public async Task<IEnumerable<Todo>> GetAllByUserIdAsync(Guid userId)
+        {
+            return await _dbContext.Todos
+                .Where(t => t.UserId == userId)
+                .ToListAsync();
+        }
+
+        public async Task AddAsync(Todo todo)
+        {
+            await _dbContext.Todos.AddAsync(todo);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Todo todo)
+        {
+            _dbContext.Todos.Update(todo);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var todo = await _dbContext.Todos.FindAsync(id);
+            if (todo != null)
+            {
+                _dbContext.Todos.Remove(todo);
+                await _dbContext.SaveChangesAsync();
+            }
+        }
+    }
+}
