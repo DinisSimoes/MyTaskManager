49482d1e0215a7e5c2fd241f5a885f5502d85360
DinisSimoes <di.f.simoes@gmail.com>
2024-12-19 23:43:29 -0300

    commands

---
+++ MyTaskManager.Application/Commands/CreateTodoCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using MyTaskManager.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTaskManager.Application.Commands
+{
+    public class CreateTodoCommand : IRequest<TodoDto>
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
